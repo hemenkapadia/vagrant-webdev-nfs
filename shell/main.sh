3393b65c1761259a1ab86439d8eb5b276174c312
Hemen Kapadia <hemen.kapadia@gmail.com>
2014-06-04 02:23:57 +0530

    Initial basebox checkin

---
+++ shell/main.sh
@@ -0,0 +1,51 @@
+#!/bin/sh
+
+# Based on https://github.com/purple52/librarian-puppet-vagrant/blob/master/shell/main.sh
+
+# Directory in which librarian-puppet should manage its modules directory
+PUPPET_DIR=/etc/puppet/
+
+# NB: librarian-puppet might need git installed. If it is not already installed
+# in your basebox, this will manually install it at this point using apt or yum
+
+$(which git > /dev/null 2>&1)
+FOUND_GIT=$?
+if [ "$FOUND_GIT" -ne '0' ]; then
+  echo 'Attempting to install git.'
+  $(which apt-get > /dev/null 2>&1)
+  FOUND_APT=$?
+  $(which yum > /dev/null 2>&1)
+  FOUND_YUM=$?
+
+  if [ "${FOUND_YUM}" -eq '0' ]; then
+    yum -q -y makecache
+    yum -q -y install git
+    echo 'git installed.'
+  elif [ "${FOUND_APT}" -eq '0' ]; then
+    apt-get -q -y update
+    apt-get -q -y install git
+    echo 'git installed.'
+  else
+    echo 'No package installer available. You may need to install git manually.'
+  fi
+else
+  echo 'git found.'
+fi
+
+if [ ! -d "$PUPPET_DIR" ]; then
+	mkdir -p $PUPPET_DIR
+	echo 'puppet directory created.'
+fi
+
+cp /vagrant/puppet/Puppetfile $PUPPET_DIR
+echo 'Puppetfile copied to puppet directory.'
+
+if [ "$(gem list -i '^librarian-puppet$')" = "false" ]; then
+  gem install librarian-puppet --verbose
+  echo 'librarian-puppet installed.'
+  cd $PUPPET_DIR && librarian-puppet install --clean --verbose
+else
+	cd $PUPPET_DIR && librarian-puppet update --verbose
+fi
+
+echo 'Shell provision completed.'
